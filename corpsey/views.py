aa8ef3c441b68431b703261394d4f3dd170a1675
Nate Beaty <nate@clixel.com>
2013-02-01 22:36:16 -0600

    show number of artists on homepage, trim logo

--- corpsey/views.py
+++ corpsey/views.py
@@ -7,8 +7,10 @@ from django.template import RequestContext
 def home(request):
     comic_set = Comic.objects.filter(active=True).order_by('-date')[:5]
     page = FlatPage.objects.get(url='/')
+    num_artists = Artist.objects.count()
     return render_to_response('home.html',  {
         'title': '',
         'page': page,
+        'num_artists': num_artists,
         'comic_set': comic_set,
         }, RequestContext(request))
