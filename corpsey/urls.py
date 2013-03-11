729a683398faab24e35ea570c86310703cee1a26
Nate Beaty <nate@clixel.com>
2013-03-11 00:39:20 -0500

    require login for /contributions/, filter out contributions user has voted on & that have no panels yet

--- corpsey/urls.py
+++ corpsey/urls.py
@@ -30,6 +30,7 @@ urlpatterns = patterns('',
     url(r'^contribute/upload/$', lambda x: redirect('/')),
     url(r'^contribute/$', 'corpsey.apps.comics.views.contribute', name='contribute'),
     url(r'^contributions/$', 'corpsey.apps.comics.views.contributions', name='contributions'),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
     # url(r'^contributions/vote/(?P<contribution_id>[\d]+)/(?P<vote>[\d]+)/$', 'corpsey.apps.comics.views.contribution_vote', name='contribution_vote'),
     
     url(r'^artist/([\d]+)/$', 'corpsey.apps.artists.views.entry', name='artist'),
