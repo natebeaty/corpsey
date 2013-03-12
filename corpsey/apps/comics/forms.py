9b5833e486feb03a97f03463d3a9696b70d87d6c
Nate Beaty <nate@clixel.com>
2013-03-11 21:12:53 -0500

    style contribute form more, better error handling + js validation

--- corpsey/apps/comics/forms.py
+++ corpsey/apps/comics/forms.py
@@ -4,11 +4,11 @@ class UploadForm(forms.Form):
     panel1 = forms.FileField(label='Select jpg for panel 1')
     panel2 = forms.FileField(label='Select jpg for panel 2')
     panel3 = forms.FileField(label='Select jpg for panel 3')
-    name = forms.CharField(label='Your Name')
-    email = forms.CharField(label='Your email')
-    website = forms.CharField(label='Your website', required=False)
+    name = forms.CharField(label='Your name', widget=forms.TextInput(attrs={'placeholder': 'Your name'}))
+    email = forms.CharField(label='Your email', widget=forms.TextInput(attrs={'placeholder': 'Your email'}))
+    website = forms.CharField(label='Your email', widget=forms.TextInput(attrs={'placeholder': 'Your email'}))
 
 class ContributeForm(forms.Form):
-    email = forms.EmailField()
-    name = forms.CharField()
+    email = forms.EmailField(label='Your name', widget=forms.TextInput(attrs={'placeholder': 'Your email', 'class': 'email required'}))
+    name = forms.CharField(label='Your email', widget=forms.TextInput(attrs={'placeholder': 'Your name', 'class': 'required'}))
     comic_id = forms.CharField()
