7641b1e4f773a36ae5ca9bc5d94fbf62f4fc0bfe
Nate Beaty <nate@clixel.com>
2013-03-12 00:02:34 -0500

    change homepage to just pull 10 recent strips again, add deadline text to contribute page

--- corpsey/apps/comics/views.py
+++ corpsey/apps/comics/views.py
@@ -135,6 +135,7 @@ def contributions(request):
 def contribute(request):
     """Reserve a spot to contribute after a comic."""
     from corpsey.apps.comics.forms import ContributeForm
+    from datetime import datetime, timedelta
 
     parent_comic = find_comic_to_follow()
     step = 1
@@ -196,11 +197,13 @@ def contribute(request):
 
     page = FlatPage.objects.get(url='/contribute/')
     page2 = FlatPage.objects.get(url='/contribute/ok/')
+    deadline = datetime.now()+timedelta(days=4)
 
     return render_to_response(
         'comics/contribute.html',
         {
             'message': message,
+            'deadline': deadline,
             'step': step,
             'page': page,
             'page2': page2,
--- corpsey/views.py
+++ corpsey/views.py
@@ -3,15 +3,11 @@ from apps.comics.models import *
 from django.shortcuts import get_object_or_404, render_to_response
 from django.contrib.flatpages.models import FlatPage
 from django.template import RequestContext
-from datetime import datetime, timedelta
 from django.views.decorators.cache import cache_page
 
 @cache_page(60 * 15)
 def home(request):
-    comic_set = Comic.objects.filter(active=True, date__gte=datetime.now()-timedelta(days=7)).order_by('-date')
-    # if less than 10 in last week, just pull last 10!
-    if len(comic_set) < 10:
-	    comic_set = Comic.objects.filter(active=True).order_by('-date')[:10]
+    comic_set = Comic.objects.filter(active=True).order_by('-date')[:10]
     page = FlatPage.objects.get(url='/')
     num_artists = Artist.objects.count()
     return render_to_response('home.html',  {
