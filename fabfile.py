65212849d2e3d70809e0783d33253d7d195d8229
Nate Beaty <nate@clixel.com>
2013-05-04 11:28:24 -0500

    update fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,12 +1,11 @@
 from fabric.api import *
 
 env.hosts = ['corpsey.trubbleclub.com']
-env.user = ['natebeaty']
 env.warn_only = True
 
 def deploy():
     with cd('/home/natebeaty/webapps/django15/corpsey/'):
-        run('/home/natebeaty/.virtualenvs/corpsey_15/bin/activate_this.py')
+        # run('/home/natebeaty/.virtualenvs/corpsey_15/bin/activate_this.py')
         run('git pull origin master')
         run('/home/natebeaty/.virtualenvs/corpsey_15/bin/python2.7 manage.py collectstatic --noinput')
         run('/home/natebeaty/.virtualenvs/corpsey_15/bin/python2.7 manage.py clear_cache')
