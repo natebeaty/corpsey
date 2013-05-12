dbb9e4aef9dec8fd62c9b4ca3924276cdc595537
Nate Beaty <nate@clixel.com>
2013-05-12 12:14:31 -0500

    go back to old subject for new submissions

--- corpsey/apps/comics/views.py
+++ corpsey/apps/comics/views.py
@@ -285,7 +285,7 @@ def contribute_upload(request, upload_code):
 
                 # Email voters about new submission
                 from django.contrib.auth.models import User, Group
-                mail_subject = 'New Infinite Corpse submission from %s!' % contribution.name
+                mail_subject = 'New Infinite Corpse submission!'
                 mail_body = 'OMG LOOK:\n\n%s\n\nGo forth Trubblers and cast your Yea or Nay: http://%s/contributions/#contribution-%s\n' % (contribution, request.META['HTTP_HOST'], contribution.id)
                 voters = User.objects.filter(groups__name='voters')
                 emails = ()
