91f50b3392b3d310df3c682adf68f5c4ac515291
Nate Beaty <nate@clixel.com>
2016-05-13 12:11:50 -0500

    initial migrations

--- corpsey/apps/artists/migrations/0001_initial.py
+++ corpsey/apps/artists/migrations/0001_initial.py
@@ -1,40 +1,30 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import easy_thumbnails.fields
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Artist'
-        db.create_table('artists_artist', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=250, blank=True)),
-            ('first_name', self.gf('django.db.models.fields.CharField')(max_length=250, blank=True)),
-            ('last_name', self.gf('django.db.models.fields.CharField')(max_length=250, blank=True)),
-            ('url', self.gf('django.db.models.fields.CharField')(max_length=250, blank=True)),
-            ('image', self.gf('django.db.models.fields.files.ImageField')(max_length=100, blank=True)),
-        ))
-        db.send_create_signal('artists', ['Artist'])
+    dependencies = [
+    ]
 
-
-    def backwards(self, orm):
-        # Deleting model 'Artist'
-        db.delete_table('artists_artist')
-
-
-    models = {
-        'artists.artist': {
-            'Meta': {'object_name': 'Artist'},
-            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '250', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'blank': 'True'}),
-            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '250', 'blank': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '250', 'blank': 'True'}),
-            'url': ('django.db.models.fields.CharField', [], {'max_length': '250', 'blank': 'True'})
-        }
-    }
-
-    complete_apps = ['artists']
+    operations = [
+        migrations.CreateModel(
+            name='Artist',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=250, blank=True)),
+                ('first_name', models.CharField(max_length=250, blank=True)),
+                ('last_name', models.CharField(max_length=250, blank=True)),
+                ('email', models.CharField(max_length=250, blank=True)),
+                ('url', models.URLField(max_length=250, blank=True)),
+                ('image', easy_thumbnails.fields.ThumbnailerImageField(upload_to=b'artists', blank=True)),
+            ],
+            options={
+                'ordering': ['last_name', 'first_name'],
+            },
+            bases=(models.Model,),
+        ),
+    ]
--- corpsey/apps/comics/migrations/0001_initial.py
+++ corpsey/apps/comics/migrations/0001_initial.py
@@ -1,57 +1,103 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import mptt.fields
+from django.conf import settings
+import easy_thumbnails.fields
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Comic'
-        db.create_table('comics_comic', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('parent', self.gf('django.db.models.fields.related.ForeignKey')(blank=True, related_name='children', null=True, to=orm['comics.Comic'])),
-            ('artist', self.gf('django.db.models.fields.related.ForeignKey')(blank=True, related_name='artists', null=True, to=orm['artists.Artist'])),
-            ('notes', self.gf('django.db.models.fields.TextField')(blank=True)),
-            ('date', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
-            ('active', self.gf('django.db.models.fields.BooleanField')(default=False)),
-            ('full_image', self.gf('django.db.models.fields.files.ImageField')(max_length=100, blank=True)),
-            ('panel1', self.gf('django.db.models.fields.files.ImageField')(max_length=100, blank=True)),
-            ('panel2', self.gf('django.db.models.fields.files.ImageField')(max_length=100, blank=True)),
-            ('panel3', self.gf('django.db.models.fields.files.ImageField')(max_length=100, blank=True)),
-        ))
-        db.send_create_signal('comics', ['Comic'])
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('artists', '0001_initial'),
+    ]
 
-
-    def backwards(self, orm):
-        # Deleting model 'Comic'
-        db.delete_table('comics_comic')
-
-
-    models = {
-        'artists.artist': {
-            'Meta': {'object_name': 'Artist'},
-            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '250', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'blank': 'True'}),
-            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '250', 'blank': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '250', 'blank': 'True'}),
-            'url': ('django.db.models.fields.CharField', [], {'max_length': '250', 'blank': 'True'})
-        },
-        'comics.comic': {
-            'Meta': {'object_name': 'Comic'},
-            'active': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'artist': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'artists'", 'null': 'True', 'to': "orm['artists.Artist']"}),
-            'date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            'full_image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'notes': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
-            'panel1': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'blank': 'True'}),
-            'panel2': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'blank': 'True'}),
-            'panel3': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'blank': 'True'}),
-            'parent': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'children'", 'null': 'True', 'to': "orm['comics.Comic']"})
-        }
-    }
-
-    complete_apps = ['comics']
+    operations = [
+        migrations.CreateModel(
+            name='Comic',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('notes', models.TextField(blank=True)),
+                ('date', models.DateTimeField(auto_now_add=True)),
+                ('active', models.BooleanField(default=False)),
+                ('starter', models.BooleanField(default=False)),
+                ('featured', models.BooleanField(default=False)),
+                ('full_image', easy_thumbnails.fields.ThumbnailerImageField(upload_to=b'comics', blank=True)),
+                ('panel1', easy_thumbnails.fields.ThumbnailerImageField(upload_to=b'comics', blank=True)),
+                ('panel2', easy_thumbnails.fields.ThumbnailerImageField(upload_to=b'comics', blank=True)),
+                ('panel3', easy_thumbnails.fields.ThumbnailerImageField(upload_to=b'comics', blank=True)),
+                ('lft', models.PositiveIntegerField(editable=False, db_index=True)),
+                ('rght', models.PositiveIntegerField(editable=False, db_index=True)),
+                ('tree_id', models.PositiveIntegerField(editable=False, db_index=True)),
+                ('level', models.PositiveIntegerField(editable=False, db_index=True)),
+                ('artist', models.ForeignKey(related_name='comics', blank=True, to='artists.Artist', null=True)),
+                ('parent', mptt.fields.TreeForeignKey(related_name='children', blank=True, to='comics.Comic', null=True)),
+                ('portal_to', models.ForeignKey(related_name='portal', blank=True, to='comics.Comic', null=True)),
+            ],
+            options={
+                'ordering': ['tree_id', 'lft'],
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Contribution',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=250, blank=True)),
+                ('email', models.CharField(max_length=250, blank=True)),
+                ('website', models.URLField(max_length=250, blank=True)),
+                ('code', models.CharField(help_text=b'Upload link is http://corpsey.trubbleclub.com/contribute/upload/CODE_HERE/', max_length=250, blank=True)),
+                ('date', models.DateTimeField(auto_now_add=True)),
+                ('deadline', models.DateTimeField(blank=True)),
+                ('panel1', easy_thumbnails.fields.ThumbnailerImageField(upload_to=b'contributions', blank=True)),
+                ('panel2', easy_thumbnails.fields.ThumbnailerImageField(upload_to=b'contributions', blank=True)),
+                ('panel3', easy_thumbnails.fields.ThumbnailerImageField(upload_to=b'contributions', blank=True)),
+                ('pending', models.BooleanField(default=True)),
+                ('has_panels', models.BooleanField(default=False)),
+                ('accepted', models.BooleanField(default=False)),
+                ('notes', models.TextField(blank=True)),
+                ('comic', models.ForeignKey(related_name='contributions', to='comics.Comic')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Rule',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('text', models.TextField()),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Uturn',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('panel', easy_thumbnails.fields.ThumbnailerImageField(upload_to=b'comics', blank=True)),
+                ('portal_to', models.ForeignKey(related_name='uturn', to='comics.Comic')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Vote',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('date', models.DateTimeField(auto_now_add=True)),
+                ('approve', models.BooleanField(default=False)),
+                ('notes', models.TextField(blank=True)),
+                ('contribution', models.ForeignKey(related_name='votes', to='comics.Contribution')),
+                ('rule_broke', models.ForeignKey(related_name='rules_broke', default=None, blank=True, to='comics.Rule', null=True)),
+                ('user', models.ForeignKey(related_name='votes', to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
