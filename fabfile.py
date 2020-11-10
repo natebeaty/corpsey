94a87d10a642e8f4e9c57dcc925efa7575bedc82
Nate Beaty <nate@clixel.com>
2020-11-09 23:47:30 -0500

    make assets optional

--- fabfile.py
+++ fabfile.py
@@ -4,12 +4,13 @@ env.hosts = ['natebeaty.opalstacked.com']
 env.user = 'natebeaty'
 env.warn_only = True
 
-def deploy():
+def deploy(assets='n'):
     with cd('/home/natebeaty/apps/corpsey/corpsey/'):
         # run('/home/natebeaty/.virtualenvs/corpsey_110/bin/activate_this.py')
         run('git pull origin master')
-        run('/home/natebeaty/apps/corpsey/env/bin/python manage.py collectstatic --noinput')
-        run('/home/natebeaty/apps/corpsey/env/bin/python manage.py clear_cache')
+        if assets != 'n':
+            run('/home/natebeaty/apps/corpsey/env/bin/python manage.py collectstatic --noinput')
+        # run('/home/natebeaty/apps/corpsey/env/bin/python manage.py clear_cache')
         restart()
 
 def syncdb():
